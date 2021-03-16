afd0f355b83e3e19b5999c64c6fefbd03f1eca54
Michael Carr <mcarr2468@gmail.com>
2021-03-16 16:41:25 -0500

    V1.3

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 let textInput = document.querySelector(".todo__input")
 let addInputBtn = document.querySelector(".add-btn")
 let todoTasks = document.querySelector(".todo__tasks")
-
+let deleteAllBtn = document.querySelector(".todo__delete-all-btn")
 
 
 
 /*TODO
 1. get input value from the input [DONE]
 
-2. add the input to the list w/ the add button
-- append the dom(ul) w/ the input
+2. add the input to the list w/ the add button [DONE]
 
-3. delete the current item in the list w/ the delete button
+3. delete the current item in the list w/ the delete button [DONE]
 
-4. delete all the items w/ the delete all btn
+4. delete all the items w/ the delete all btn [DONE]
+
+5. put the todo tasks into local storage
 */
 
 
@@ -21,28 +22,17 @@ let todoTasks = document.querySelector(".todo__tasks")
 
 
 /*get the user input value from the input box*/
-// function userInput(event) {
-//     return event.target.value;
-// }
-
-
 function userInput() {
     return textInput.value;
 }
-
-
 textInput.addEventListener("input", userInput);
 
 
 
 
-/*add the input to the list w/ the add button
------
-OR enter keypress
-(do enter button as after every other step)*/
-function addUserInputToTodoList() {
-    // let userInput = userInput();
 
+/*add the input to the list w/ the add button*/
+function addUserInputToTodoList() {
     /*fragment*/
         const docFrag = document.createDocumentFragment();
         let todoTask = document.createElement("li");
@@ -56,17 +46,50 @@ function addUserInputToTodoList() {
         todoTaskText.classList.add("todo__task__text");
         todoTaskDeleteBtn.classList.add("todo__task__delete-btn");
         todoTaskDeleteBtnIcon.classList.add("fa", "fa-times");
-
         /*append the items*/
         docFrag.appendChild(todoTask);
         todoTask.appendChild(todoTaskText);
         todoTask.appendChild(todoTaskDeleteBtn);
         todoTaskDeleteBtn.appendChild(todoTaskDeleteBtnIcon);
-
-        /*append the document fragment to the DOM(todo__tasks)*/
-        todoTasks.appendChild(docFrag);
+    /*append the document fragment to the DOM(todo__tasks)*/
+    todoTasks.appendChild(docFrag);
 }
-
 addInputBtn.addEventListener("click", addUserInputToTodoList);
+
+
+
+
+
+/*delete the current item in the list w/ the delete button */
+function deleteTaskBtn(event) {
+    let deleteBtn = event.target.closest(".todo__task__delete-btn");
+    let icon = event.target.closest(".fa-times");
+    let currentItem = event.target.closest(".todo__task");
+
+
+    // check if your currently clicking on the delete button
+    if(event.target === icon || event.target === deleteBtn) {
+        let confirmTaskDeletion = confirm("Do you want to delete this task?");
+        if(confirmTaskDeletion) {
+            currentItem.remove();
+        }
+    }
+}
+todoTasks.addEventListener("click", deleteTaskBtn);
+
+
+
+
+
+// delete all tasks
+deleteAllBtn.addEventListener("click", _ => {
+    let todoTaskAll = Array.from(document.querySelectorAll(".todo__task"))
+    for(let i = 0; i < todoTaskAll.length; i++) {
+        todoTaskAll[i].remove()
+    }
+})
+
+
+
 
 

72286084898963595915b8909f66db01aa053c3a
Michael Carr <mcarr2468@gmail.com>
2021-03-15 20:45:04 -0500

    V1.0

---
+++ app.js
@@ -0,0 +1,16 @@
+let textInput = document.querySelector(".todo__input")
+let addInputBtn = document.querySelector(".add-btn")
+let todoTasks = document.querySelector(".todo__tasks")
+
+
+
+
+/*TODO
+1. get input from the input
+
+2. add the input to the list w/ the add button
+
+3. delete the current item in the list w/ the delete button
+
+4. delete all the items w/ the delete all btn
+*/
